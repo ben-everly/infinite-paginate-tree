abc224e59c25b9d2a5b54307b302c0794632a28a
Ben Everly <ben.everly@oberd.com>
2025-08-14 20:09:59 -0500

    nodes migration

--- database/migrations/2025_07_29_032025_create_nodes_table.php
+++ database/migrations/2025_07_29_032025_create_nodes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Node;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('nodes', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Node::class, 'parent_id')->nullable()->constrained();
+            $table->string('path')->nullable()->index();
             $table->timestamps();
         });
     }
