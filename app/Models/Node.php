066bdb4628a592196e1f1241355c223d0dda4b59
Ben Everly <ben.everly@oberd.com>
2025-08-14 20:04:18 -0500

    `Node` model

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Node extends Model
 {
-    //
+    protected $fillable = ['parent_id'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function (self $node) {
+            $pathIndex = $node->parent
+                ? $node->parent->children()->max('path')
+                : Node::whereNull('parent_id')->max('path');
+            $pathIndex = collect(explode('/', $pathIndex))
+                ->filter()
+                ->last() + 1;
+            $node->path = ($node->parent?->path ?? '/')
+                .str_pad($pathIndex, 5, '0', STR_PAD_LEFT).'/';
+            $node->save();
+        });
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Node::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Node::class, 'parent_id');
+    }
 }
